224bb91b46482613903f591d4a7c4c6f68a59fa9
Zimny1 <matmateo2008@gmail.com>
2025-02-17 13:34:51 +0100

    Add files via upload

---
+++ c# kosci do gry MMróz/kosci do gry/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosci_do_gry
+{
+    internal class Program
+    {
+        static int[] Rzuty(int ile)
+        {
+            Random rnd = new Random();
+            int[] kosci = new int[ile];
+
+            for (int i = 0; i < ile; i++)
+            {
+                kosci[i] = rnd.Next(1, 7);
+            }
+
+            return kosci;
+        }
+        static int Punkty(int[] kosci)
+        {
+            int punkty=0;
+            int[] mozliwosci = {1,2,3,4,5,6};
+            int[] powtorzenia = {0,0,0,0,0,0};
+
+            for (int i = 0; i < kosci.Length; i++)
+            {
+                for (int j = 0; j < mozliwosci.Length;j++)
+                {
+                    if (kosci[i] == mozliwosci[j])
+                    {
+                        powtorzenia[j]++;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < mozliwosci.Length; i++)
+            {
+                if (powtorzenia[i]>=2)
+                {
+                    punkty += powtorzenia[i] * mozliwosci[i];
+                }
+            }
+
+            
+            return punkty;
+        }
+        static void Main(string[] args)
+        {
+            string odp;
+            int ile;
+
+            do
+            {
+                Console.WriteLine("Podaj ilość kości 3-10");
+                ile = int.Parse(Console.ReadLine());
+            }
+            while (ile < 3 || ile > 10);
+
+            do
+            {
+                int[] kosci = Rzuty(ile);
+
+                for (int i = 0; i < ile; i++)
+                {
+                    Console.WriteLine("Kostka " + (i+1) + ": " + kosci[i]);
+                }
+
+                int punkty = Punkty(kosci);
+
+                Console.WriteLine($"Wyrzuciłeś {punkty} punktów.");
+
+                Console.WriteLine("Powtórzyć losowanie? (t/n)");
+                odp = Console.ReadLine();
+            }while (odp=="t");
+        }
+    }
+}
---
+++ c# kosci do gry MMróz/kosci do gry/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("kosci do gry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kosci do gry")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("a9a6c3b5-4bed-4300-9e60-046fe2dca452")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
